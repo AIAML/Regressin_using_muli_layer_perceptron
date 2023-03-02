d27bd447db0ac725670f96ab7f9dad1612c85e23
Majid Hemmati <majid.hemmati@yahoo.com>
2023-03-02 22:14:21 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import matplotlib.pyplot as plt
+
+housing = fetch_california_housing()
+x_train_full, x_test, y_train_full, y_test = train_test_split(housing.data, housing.target)
+x_train, x_valid, y_train, y_valid = train_test_split(x_train_full, y_train_full)
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_valid = scaler.transform(x_valid)
+x_test = scaler.transform(x_test)
+
+model = keras.models.Sequential([
+keras.layers.Dense(30, activation="relu", input_shape=x_train.shape[1:]),
+keras.layers.Dense(1)
+])
+
+model.compile(loss="mean_squared_error", optimizer="sgd")
+
+history = model.fit(x_train, y_train, epochs=20, validation_data=(x_valid, y_valid))
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1) # set the vertical range to [0-1]
+plt.show()
